4bfffea1c0c043e3e7359f432c2f2309c0a9e9ec
Harry <wuxiaohuaer@gmail.com>
2017-03-21 23:42:32 +0800

    ex2-1 V0.2:按键命令都加上去了, 剩余的实际电压电流输出值未显示(需要用定时器实时更新).

--- C# WinForm Works/Excercises in  DoYang C# course/ex2-1 SerialPort Switch/ex2-1 SerialPort Switch/Form1.cs
+++ C# WinForm Works/Excercises in  DoYang C# course/ex2-1 SerialPort Switch/ex2-1 SerialPort Switch/Form1.cs
@@ -8,25 +8,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ex2_1_SerialPort_Switch.Properties;
 
 namespace ex2_1_SerialPort_Switch
 {
     public partial class Form1 : Form
     {
+        bool _outputStatue = false;  //输出状态flag
         public Form1()
         {
             InitializeComponent();
             this.textBox3.KeyDown += new KeyEventHandler(textBox3_KeyDown);//这句不能省
+            this.textBox4.KeyDown += new KeyEventHandler(textBox4_KeyDown);//这句不能省
+            this.textBox7.KeyDown += new KeyEventHandler(textBox7_KeyDown);//这句不能省
+            this.textBox8.KeyDown += new KeyEventHandler(textBox8_KeyDown);//这句不能省
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived); //必须手动添加事件处理程序
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnSend_Click(object sender, EventArgs e)
         {
+            WriteStringToSerialPort("", txtSend.Text);
 
         }
 
@@ -46,6 +53,9 @@ namespace ex2_1_SerialPort_Switch
                 catch { }
                 btnSerial.Text = "打开串口";
                 btnSerial.BackColor = Color.OrangeRed;
+                btnOutput.BackgroundImage = Resources.OFF;  //关
+                _outputStatue = false;
+
             }
             else
             {
@@ -64,25 +74,25 @@ namespace ex2_1_SerialPort_Switch
         }
 
         //扫描并将可用串口添加至下拉列表
-        private void SearchAndAddSerialToComboBox(SerialPort MyPort, ComboBox MyBox)
+        private void SearchAndAddSerialToComboBox(SerialPort MyPort, ComboBox myBox)
         {
-            var MyString = new string[20];
-            string Buffer;  //缓存
-            MyBox.Items.Clear();
+            var myString = new string[20];
+            string buffer;  //缓存
+            myBox.Items.Clear();
             for (int i = 1; i < 21; i++)
             {
                 try
                 {
-                    Buffer = "COM" + i.ToString();
-                    MyPort.PortName = Buffer;
+                    buffer = "COM" + i.ToString();
+                    MyPort.PortName = buffer;
                     MyPort.Open();  ////如果失败，后面的代码不会执行,直接跳到catch
-                    MyString[i - 1] = Buffer;
-                    MyBox.Items.Add(Buffer); //打开成功，添加至下拉列表
+                    myString[i - 1] = buffer;
+                    myBox.Items.Add(buffer); //打开成功，添加至下拉列表
                     MyPort.Close();
                 }
                 catch {}
             }
-            MyBox.Text = MyString[0]; //扫描完所有的端口后,将第一个可用端口添加到ComboBox
+            myBox.Text = myString[0]; //扫描完所有的端口后,将第一个可用端口添加到ComboBox
         }
 
         //设置电压电流的函数
@@ -92,12 +102,55 @@ namespace ex2_1_SerialPort_Switch
         {
             if (e.KeyCode == Keys.Enter)
             {
-                MessageBox.Show("Test");
+                WriteStringToSerialPort(Convert.ToString(textBox3.Tag), textBox3.Text);
+                //MessageBox.Show("Test");
+            }
+        }
+        void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                WriteStringToSerialPort(Convert.ToString(textBox4.Tag), textBox4.Text);
+                //MessageBox.Show("Test");
+            }
+        }
+        void textBox7_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                WriteStringToSerialPort(Convert.ToString(textBox7.Tag), textBox7.Text);
+                //MessageBox.Show("Test");
+            }
+        }
+        void textBox8_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                WriteStringToSerialPort(Convert.ToString(textBox8.Tag), textBox8.Text);
+                //MessageBox.Show("Test");
             }
         }
 
         private void btnOutput_Click(object sender, EventArgs e)
         {
+
+            if (serialPort1.IsOpen) //如果串口已打开
+            {
+                if (_outputStatue)  //如果输出开
+                {
+                    WriteStringToSerialPort("OUT", "0");
+                    btnOutput.BackgroundImage = Resources.OFF;  //灭
+                    _outputStatue = false;
+                    textBox1.Text = "24";
+                }
+                else
+                {
+                    WriteStringToSerialPort("OUT", "1");
+                    btnOutput.BackgroundImage = Resources.ON;  //开
+                    _outputStatue = true;
+                    textBox2.Text = "3.1";
+                }
+            }
 
         }
 
@@ -108,21 +161,68 @@ namespace ex2_1_SerialPort_Switch
 
         private void radioButton1_Click(object sender, EventArgs e)
         {
-            if (serialPort1.IsOpen)
+            WriteStringToSerialPort("TRACK", Convert.ToString(radioButton1.Tag));
+
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            WriteStringToSerialPort("TRACK", Convert.ToString(radioButton2.Tag));
+
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            WriteStringToSerialPort("TRACK", Convert.ToString(radioButton3.Tag));
+
+            
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e) //串口数据接收事件
+        {
+                var str = serialPort1.ReadExisting(); //字符串方式读
+                txtReceive.AppendText(str);  //读取的字符显示到接收文本框
+           
+        }
+
+        private void WriteStringToSerialPort(string prefix, string data)                   //单字节写入串口
+        {
+            var buffer = prefix + data;
+            //var Buffer = new string[prefix.Length + data.Length];
+            //var Buffer = new string("VSET" + data);
+            if (serialPort1.IsOpen)                                     //传输数据的前提是端口已打开
             {
                 try
                 {
-                    serialPort1.WriteLine("TRACK0");//写数据,INDEP mode
+                    serialPort1.WriteLine(buffer);                    //写数据
                 }
                 catch
                 {
-                    MessageBox.Show("数据写入错误", "错误");//出错提示
+                    MessageBox.Show("串口数据发送出错，请检查.", "错误");//错误处理
+                    serialPort1.Close();
+                    btnSerial.Text = "打开串口";
+                    btnSerial.BackColor = Color.OrangeRed;
+                    //btnOutput.BackgroundImage = Resources.OFF;  //关
+
+
                 }
             }
             else
             {
                 MessageBox.Show("请先打开串口", "错误");
+                //btnOutput.BackgroundImage = Resources.OFF;  //关
+
             }
+        }
+
+        private void txtReceive_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
